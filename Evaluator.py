b78f029b85f8e5f25b9752da432c6428000f55df
Sbrussee <Siemenbrussee@hotmail.com>
2019-03-06 14:24:49 +0100

    Added perfomance plot function, commented out unnecessary functions for now.

--- Evaluator.py
+++ Evaluator.py
@@ -12,19 +12,23 @@ import matplotlib.pyplot as plt
 class Evaluator:
     #Iniatior function,sets metrics to empty string in order to be able to
     #check if these metrics are already calculated.
-    def __init__(self, real_annotation, pred_annotation):
+    def __init__(self, desired_metric, real_annotation, pred_annotation,
+                 missing_data_array, performance_array):
         #Set input files/lists
         self.real_annotation = real_annotation
         self.pred_annotation = pred_annotation
-        #Set metrics.
-        self.average_accuracy = ''
-        self.average_precision = ''
-        self.average_recall = ''
-        self.average_f_measure = ''
-        self.max_f_measure = ''
-        #Set binary inputs.
-        self.binary_real_annotation = ''
-        self.binary_pred_annotation = ''
+        self.desired_metric = desired_metric
+        self.missing_data_array = missing_data_array
+        self.performance_array = performance_array
+        # #Set metrics.
+        # self.average_accuracy = ''
+        # self.average_precision = ''
+        # self.average_recall = ''
+        # self.average_f_measure = ''
+        # self.max_f_measure = ''
+        # #Set binary inputs.
+        # self.binary_real_annotation = ''
+        # self.binary_pred_annotation = ''
 
 
     #Function usable to get input in proper format.
@@ -37,57 +41,47 @@ class Evaluator:
     #       correct_pred.append(line.split(" ")[1]) #Would be a list of pred GO-terms
     #   self.real_annotation = correct_real
     #   self.pred_annotation = correct_pred
-
+    #
     #Standard accuracy method.
-    def get_accuracy(self):
-        #Check whether metric is already calculated.
-        if len(self.average_accuracy) == 0:
-            #Calculate metric and return it.
-            return metrics.accuracy_score(self.real_annotation, self.pred_annotation)
-        else:
-            #Return previously calculated metric.
-            return self.average_accuracy
+    # def get_accuracy(self):
+    #     #Check whether metric is already calculated.
+    #     if len(self.average_accuracy) == 0:
+    #         #Calculate metric and return it.
+    #         return metrics.accuracy_score(self.real_annotation, self.pred_annotation)
+    #     else:
+    #         #Return previously calculated metric.
+    #         return self.average_accuracy
 
     #Precision method, calculates average precision from a single function.
-    def get_precision(self):
-        #Check whether metric is already calculated.
-        if len(self.average_precision) == 0:
-            #Calculate metric and return it.
-            return metrics.average_precision_score(self.real_annotation, self.pred_annotation)
-        else:
-            #Return previously calculated metric.
-            return self.average_precision
+    # def get_precision(self):
+    #     #Check whether metric is already calculated.
+    #     if len(self.average_precision) == 0:
+    #         #Calculate metric and return it.
+    #         return metrics.average_precision_score(self.real_annotation, self.pred_annotation)
+    #     else:
+    #         #Return previously calculated metric.
+    #         return self.average_precision
 
-    #Recall method
-    def get_recall(self):
-        #Check whether metric is already calculated.
-        if len(self.average_recall) == 0:
-            #Calculate metric and return it.
-            return metrics.recall_score(self.real_annotation, self.pred_annotation)
-        else:
-            #Return previously calculated metric.
-            return self.average_recall
+    # #F-measure method, f-max needs to be added.
+    # def get_f(self):
+    #     #Check whether metric is already calculated.
+    #     if len(self.average_f_measure) == 0:
+    #         #Calculate metric and return it.
+    #         return metrics.f1_score(self.real_annotation, self.pred_annotation)
+    #     else:
+    #         #Return previously calculated metric.
+    #         return self.f
 
-    #F-measure method, f-max needs to be added.
-    def get_f(self):
-        #Check whether metric is already calculated.
-        if len(self.average_f_measure) == 0:
-            #Calculate metric and return it.
-            return metrics.f1_score(self.real_annotation, self.pred_annotation)
-        else:
-            #Return previously calculated metric.
-            return self.f
-
-    #Binarize-method in order for data to be applicable in ROC-curve methods.
-    def binarize_input(self):
-        #Check whether input is already binarized:
-        if len(self.binary_real_annotation) == 0:
-            #Binarize real annotation and predicted annotation.
-            self.binary_real_annotation = preprocessing.binarize(self.real_annotation)
-            self.binary_pred_annotation = preprocessing.binarize(self.pred_annotation)
-        else:
-            #Do nothing if input was already binarized.
-            pass
+    # #Binarize-method in order for data to be applicable in ROC-curve methods.
+    # def binarize_input(self):
+    #     #Check whether input is already binarized:
+    #     if len(self.binary_real_annotation) == 0:
+    #         #Binarize real annotation and predicted annotation.
+    #         self.binary_real_annotation = preprocessing.binarize(self.real_annotation)
+    #         self.binary_pred_annotation = preprocessing.binarize(self.pred_annotation)
+    #     else:
+    #         #Do nothing if input was already binarized.
+    #         pass
 
     #INPUT NEEDS TO BE BINARIZED IN ORDER TO WORK FOR MULTI-LABEL CLASSIFICATION!
     #GO-centric ROC evalution:
@@ -99,11 +93,19 @@ class Evaluator:
     #
     #
 
-    def get_all_evaluations(self):
-        #Return all available metrics by calling their respective functions:
-        return "Average accuracy:\t%d\n" \
-               "Average Precision:\t%d\n" \
-               "Average Recall:\t%d\n" \
-               "Average F-measure:\t%d\n" \
-               % (self.get_accuracy(), self.get_precision(),
-                  self.get_recall(), self.get_f())
+    # def get_all_evaluations(self):
+    #     #Return all available metrics by calling their respective functions:
+    #     return "Average accuracy:\t%d\n" \
+    #            "Average Precision:\t%d\n" \
+    #            "Average Recall:\t%d\n" \
+    #            "Average F-measure:\t%d\n" \
+    #            % (self.get_accuracy(), self.get_precision(),
+    #               self.get_recall(), self.get_f())
+
+    def plot_performance(self):
+        perf_plot, perf_ax = plt.subplots()
+        perf_ax.plot(self.missing_data_array, self.performance_array)
+        plt.xlabel("Percentage of missing data")
+        plt.ylabel("Model performance")
+        plt.title("PFP-model performance under varying missing data fractions")
+        plt.show()
