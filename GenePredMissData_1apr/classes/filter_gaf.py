e8526f15796bc036b0e7d12b6e21a1b69fd6eb01
Sbrussee <Siemenbrussee@hotmail.com>
2019-04-04 11:03:01 +0200

    *Removed test print statements from filter_gaf.py

--- GenePredMissData_1apr/classes/filter_gaf.py
+++ GenePredMissData_1apr/classes/filter_gaf.py
@@ -4,10 +4,8 @@ def filter_gaf(gaf_lines, evidence_codes=['EXP', 'IDA', 'IPI', 'IMP', 'IGI', 'IE
                   'IGC', 'RCA', 'TAS', 'NAS', 'IC', 'ND',
                   'IEA']):
     headfiltered_gaf = gaf_lines[12:]
-    print(len(headfiltered_gaf))
     for index, line in enumerate(headfiltered_gaf):
         line_items = line.split("\t")
         if line_items[3] != "" or line_items[6] not in evidence_codes:
             del headfiltered_gaf[index]
-    print(len(headfiltered_gaf))
     return headfiltered_gaf
