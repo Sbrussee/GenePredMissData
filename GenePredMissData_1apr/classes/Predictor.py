2ebf240dee9c3e354e9109893b8d7d6b7699d943
theschutter <47389253+theschutter@users.noreply.github.com>
2019-04-08 19:55:24 +0200

    Update Predictor.py

--- GenePredMissData_1apr/classes/Predictor.py
+++ GenePredMissData_1apr/classes/Predictor.py
@@ -1,23 +1,24 @@
 import pandas as pd
 import numpy as np
+
+# Class predictor:
+# 1. Determine predictor method, e.g. blast best hit
+# 2. Get the train data for the used method.
+# 3. get the train gaf file.
+# 4. Extend the go-terms for the train data
+# 5. If blast method > 1 best hit then calculate frequence.
+
+
+# Part 1: the predictor method.
 class Predictor:
     # Import the train data and get all blast results for the best blast hit or blast extension.
+    # Temporary extend: Can be changed to predictor method.
     def __init__(self, traindata, extend):
         self.extend = extend
-        self.traindata = {}
-        remember, count = "", 0
-        for line in traindata:
-            id = line.split("\t")
-            input = id[0].strip()
-            output = id[-1].strip()
-            if input == remember and count < extend:
-                self.traindata[input].append(output)
-                count += 1
-            elif input != remember:
-                remember = input
-                self.traindata[input] = []
-                self.traindata[input].append(output)
-                count = 1
+        if self.extend == 1:
+            self.traindata = get_lijst_no_extend(self, traindata)
+        else:
+            self.traindata = get_lijst_extend(self, traindata, extend)
 
 
     # Import the train gaf file.
@@ -35,7 +36,43 @@ class Predictor:
             prediction = get_blast_extend(self.trainclass, self.traindata, testdata)
         return prediction
 
+
+
+# Part 2: Functions depending on blast method in predictor.
+
+
+# Get traindata if the best blast hit score is 1
+# self.traindata = traindata
+def get_lijst_no_extend(self, traindata):
+    self.traindata = {}
+    for line in traindata:
+        line = line.split("\t")
+        if len(line) > 1:
+            self.traindata[line[0]] = line[1].strip()
+    return self.traindata
+
+# Get traindata if the best blast hit score is > 1
+# self.traindata = traindata
+def get_lijst_extend(self, traindata, extend):
+    self.traindata = {}
+    remember, count = "", 0
+    for line in traindata:
+        id = line.split("\t")
+        input = id[0].strip()
+        output = id[-1].strip()
+        if input == remember and count < extend:
+            self.traindata[input].append(output)
+            count += 1
+        elif input != remember:
+            remember = input
+            self.traindata[input] = []
+            self.traindata[input].append(output)
+            count = 1
+    return self.traindata
+
+
 # Get prediction if blast extension
+# Prediction = id + go termen + frequence per go term
 def get_blast_extend(clas, train, test):
     predictions = {}
     for protein in test:
@@ -48,18 +85,19 @@ def get_blast_extend(clas, train, test):
                 if prot in clas:
                     extend.get_blast_extend(clas[prot])
                     doorgaan = True
-
             if doorgaan:
                 predictions[protein] = extend.set_blast_extend()
     return predictions
 
+
 # get prediction if no blast extension
+# Prediction is id + go termen   (without frequence)
 def get_blast(clas, train, test):
     predictions = {}
     for protein in test:
         protein = protein.strip()
         if protein in train:
-            protein_class = train[protein][0]
+            protein_class = train[protein]
             if protein_class in clas:
                 predictions[protein] = clas[protein_class]
             else:
@@ -69,12 +107,20 @@ def get_blast(clas, train, test):
     return predictions
 
 
+
+
+# Part 3: Classe to calculate frequence if blast hit > 1:
+
+
 # If blast extension calculate the precision for each protein.
+# 1. save all predicted ids for each true protein
+# 2. Return frequentie list per predicter protein per true protein.
 class extend_converter:
     def __init__(self):
         self.blast_extend = []
         self.count = 0
 
+    # Save uniprot id in temporary list new.
     def get_blast_extend(self, blast_extend):
         new = []
         if len(blast_extend) > 1:
@@ -86,6 +132,8 @@ class extend_converter:
             self.blast_extend.append(blast_extend)
         self.count += 1
 
+    # Calculate frequence go terms for als go terms per true in in temporary list new.
+    # Use pandas to set all go terms in list and calculate values.
     def set_blast_extend(self):
         array = np.array([])
         for lines in self.blast_extend:
