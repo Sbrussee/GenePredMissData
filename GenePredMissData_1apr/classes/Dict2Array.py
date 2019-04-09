c2e14e168aa00d45f59ea3e07f9a0c2d33faddf9
theschutter <robinschutter97@gmail.com>
2019-04-09 08:20:55 +0200

    commit

--- GenePredMissData_1apr/classes/Dict2Array.py
+++ GenePredMissData_1apr/classes/Dict2Array.py
@@ -6,103 +6,50 @@ from scipy import *
 class Dict2Array:
     def __init__(self, x, y, extend):
         self.extend = extend
-        x = sorted(list(set(x)))
-        y = sorted(list(set(y)))
-        self.x_pos = {}
-        self.y_pos = {}
+        self.x_pos = {key:pos for pos, key in enumerate(x)}
+        self.y_pos = {key:pos for pos, key in enumerate(y)}
         self.x_size = len(x)
         self.y_size = len(y)
-        pos = 0
-        for key in x:
-            self.x_pos[key] = pos
-            pos += 1
-        pos = 0
-        for key in y:
-            self.y_pos[key] = pos
-            pos += 1
 
 
-    def make_array(self, data, func=None):
-        parse_matrix = []
-        if self.extend == 1:
-            parse_matrix = make_array_not_extend(self, data, func)
-        elif self.extend > 1:
-            parse_matrix = make_array_extend(self, data, func)
-        return parse_matrix
+    def make_array(self, data, func):
+        create = create_array()
+        for id, value in data.items():
 
-def make_array_extend(self, data, func):
-    parse_matrix = []
-    for key in data.values():
-        if len(key) > 0:
-            if type(key[1]) == tuple:
-                parse_matrix = train_extend(self, data, func)
-                break
+            if func != None:
+                vals = func(data[id])
             else:
-                parse_matrix = test_extend(self, data, func)
-                break
-    return parse_matrix
+                vals = func(data[id])
 
-def train_extend(self, data, func):
-    res = lil_matrix((self.y_size, self.x_size), dtype=float)
-    for key in data:
-        if func != None:
-            vals = func(data[key])
-        else:
-            vals = data[key]
-        for value in vals:
-            if not key in self.y_pos:
-                # print("Err key '%s' not in y index."%key)
-                continue
-            if not value[0] in self.x_pos:
-                print("Err value '%s' not in x index." % value[0])
-                res[self.y_pos[key], self.x_pos[value[0]]] = 0
-                continue
-            res[self.y_pos[key], self.x_pos[value[0]]] = value[1]
+            if len(value) > 0:
+                if type(value[0]) == tuple:
+                    res = create.get_extend(self.y_size, self.x_size, value, self.y_pos, self.x_pos, vals, id)
+                else:
+                    res = create.get_not_extend(self.y_size, self.x_size, self.y_pos, self.x_pos, vals, id)
+        return res
 
-    return res
+class create_array():
+    @staticmethod
+    def get_not_extend(y_size, x_size, y_pos, x_pos, vals, id):
+        res = lil_matrix((y_size, x_size), dtype=bool)
+        for val in vals:
+            if id in y_pos and val in x_pos:
+                res[y_pos[id], x_pos[val]] = True
+        return res
 
 
-def test_extend(self, data, func):
-    res = lil_matrix((self.y_size, self.x_size), dtype=float)
-    for key in data:
-        if func != None:
-            vals = func(data[key])
-        else:
-            vals = data[key]
-        for value in vals:
-            if not key in self.y_pos:
-                # print("Err key '%s' not in y index."%key)
-                continue
-            if not value in self.x_pos:
-                print("Err value '%s' not in x index." % value)
-                continue
-            res[self.y_pos[key], self.x_pos[value]] = 1
-    return res
+    @staticmethod
+    def get_extend(y_size, x_size, value, y_pos, x_pos, vals, id):
+        res = lil_matrix((y_size, x_size), dtype=float)
+        for val in vals:
+            for regels in value:
+                print(regels)
+                if id in y_pos and val in x_pos:
+                    res[y_pos[id], x_pos[val]] = regels[1]
+        return res
 
 
-def make_array_not_extend(self, data, func):
-    # res = np.full((self.y_size, self.x_size), False, dtype=bool)
-    res = lil_matrix((self.y_size, self.x_size), dtype=bool)
-    for key in data:
-        if func != None:
-            vals = func(data[key])
-        else:
-            vals = data[key]
-        for value in vals:
-            if not key in self.y_pos:
-                # print("Err key '%s' not in y index."%key)
-                continue
-            if not value in self.x_pos:
-                print("Err value '%s' not in x index." % value)
-                continue
-            res[self.y_pos[key], self.x_pos[value]] = True
-    return res
 
 
-if __name__ == "__main__":
-    x = ["GO:1", "GO:2", "GO:3"]
-    y = ["PROT1", "PROT2", "PROT3"]
-    data = {"PROT1": ["GO:1", "GO:3"], "PROT2": ["GO:2"], "PROT3": ["GO:3"]}
 
-    arrmaker = Dict2Array(x, y, 1)
-    array = arrmaker.make_array(data)
+
--- GenePredMissData_1apr/classes/Predictor.py
+++ GenePredMissData_1apr/classes/Predictor.py
@@ -9,108 +9,69 @@ import numpy as np
 # 5. If blast method > 1 best hit then calculate frequence.
 
 
-# Part 1: the predictor method.
+
 class Predictor:
     # Import the train data and get all blast results for the best blast hit or blast extension.
     # Temporary extend: Can be changed to predictor method.
     def __init__(self, traindata, extend):
-        self.extend = extend
-        if self.extend == 1:
-            self.traindata = get_lijst_no_extend(self, traindata)
-        else:
-            self.traindata = get_lijst_extend(self, traindata, extend)
+        # Call the blast method classe
+        self.blast = blast_method()
 
+        # extend is temporary to define the blast method
+        self.extend = extend
+        if extend > 0:
+            self.traindata = self.blast.get_blast_data(traindata)
 
     # Import the train gaf file.
     def set_trainclass(self, trainclass):
-        global train
-        train = trainclass
         self.trainclass = trainclass
 
     # Get prediction for or blast extension or best blast hit.
     def get_predictions(self, testdata):
-        prediction = {}
-        if self.extend == 1:
-            prediction = get_blast(self.trainclass, self.traindata, testdata)
-        if self.extend > 1:
-            prediction = get_blast_extend(self.trainclass, self.traindata, testdata)
+        if self.extend > 0:
+            prediction = self.blast.get_prediction(self.trainclass, self.traindata, testdata)
         return prediction
 
 
 
-# Part 2: Functions depending on blast method in predictor.
-
-
-# Get traindata if the best blast hit score is 1
-# self.traindata = traindata
-def get_lijst_no_extend(self, traindata):
-    self.traindata = {}
-    for line in traindata:
-        line = line.split("\t")
-        if len(line) > 1:
-            self.traindata[line[0]] = line[1].strip()
-    return self.traindata
-
-# Get traindata if the best blast hit score is > 1
-# self.traindata = traindata
-def get_lijst_extend(self, traindata, extend):
-    self.traindata = {}
-    remember, count = "", 0
-    for line in traindata:
-        if line.strip() != "":
-            id = line.split("\t")
-            input = id[0].strip()
+# De static functions to create the blast method
+class blast_method:
+    # Get the blast results
+    @staticmethod
+    def get_blast_data(traindata):
+        train = {}
+        for id in traindata:
+            id = id.split("\t")
+            input = id[0]
             output = id[-1].strip()
-            if input == remember and count < extend:
-                self.traindata[input].append(output)
-                count += 1
-            elif input != remember:
-                remember = input
-                self.traindata[input] = []
-                self.traindata[input].append(output)
-                count = 1
-    return self.traindata
+            if input in train:
+                train[input].append(output)
+            elif input not in train:
+                train[input] = []
+                train[input].append(output)
+        return train
 
+    # Create the prediction set
+    @staticmethod
+    def get_prediction(trainclas, traindata, testdata):
+        predictions = {}
+        for protein in testdata:
+            protein = protein.strip()
+            if protein in traindata:
+                protein_class = traindata[protein]
+                if len(protein_class) > 1:
+                    extend = extend_converter()
+                    for prot in protein_class:
+                        if prot in trainclas:
+                            extend.get_blast_extend(trainclas[prot])
+                    predictions[protein] = extend.set_blast_extend()
 
-# Get prediction if blast extension
-# Prediction = id + go termen + frequence per go term
-def get_blast_extend(clas, train, test):
-    predictions = {}
-    for protein in test:
-        doorgaan = False
-        protein = protein.strip()
-        if protein in train:
-            protein_class = train[protein]
-            extend = extend_converter()
-            for prot in protein_class:
-                if prot in clas:
-                    extend.get_blast_extend(clas[prot])
-                    doorgaan = True
-            if doorgaan:
-                predictions[protein] = extend.set_blast_extend()
-    return predictions
+                else:
+                    protein_class = protein_class[0]
+                    if protein_class in trainclas:
+                        predictions[protein] = trainclas[protein_class]
+        return predictions
 
-
-# get prediction if no blast extension
-# Prediction is id + go termen   (without frequence)
-def get_blast(clas, train, test):
-    predictions = {}
-    for protein in test:
-        protein = protein.strip()
-        if protein in train:
-            protein_class = train[protein]
-            if protein_class in clas:
-                predictions[protein] = clas[protein_class]
-            else:
-                predictions[protein] = []
-        else:
-            predictions[protein] = []
-    return predictions
-
-
-
-
-# Part 3: Classe to calculate frequence if blast hit > 1:
 
 
 # If blast extension calculate the precision for each protein.
@@ -118,30 +79,25 @@ def get_blast(clas, train, test):
 # 2. Return frequentie list per predicter protein per true protein.
 class extend_converter:
     def __init__(self):
-        self.blast_extend = []
+        self.blast_extend = np.array([])
         self.count = 0
 
     # Save uniprot id in temporary list new.
     def get_blast_extend(self, blast_extend):
         new = []
         if len(blast_extend) > 1:
-            for regels in blast_extend:
-                if regels not in new:
-                    new.append(regels)
-            self.blast_extend.append(new)
+            for regel in blast_extend:
+                if regel not in new:
+                    new.append(regel)
+                    self.blast_extend = np.append(self.blast_extend, regel)
         else:
-            self.blast_extend.append(blast_extend)
+            self.blast_extend = np.append(self.blast_extend, blast_extend)
         self.count += 1
 
     # Calculate frequence go terms for als go terms per true in in temporary list new.
     # Use pandas to set all go terms in list and calculate values.
     def set_blast_extend(self):
-        array = np.array([])
-        for lines in self.blast_extend:
-            for regels in lines:
-                array = np.append(array, regels)
-
-        check = pd.DataFrame({'a': array})
+        check = pd.DataFrame({'a': self.blast_extend})
         frame = check['a'].sort_values().value_counts() / self.count
         self.count = 0
         return list(zip(frame.index, frame.values))
