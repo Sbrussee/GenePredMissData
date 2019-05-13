b0d06f34297662b3d28239d74532d43321cec472
theschutter <47389253+theschutter@users.noreply.github.com>
2019-05-13 19:31:19 +0200

    domain C eruit

--- GenePredMissData_1apr/classes/filter_gaf.py
+++ GenePredMissData_1apr/classes/filter_gaf.py
@@ -2,6 +2,6 @@ def filter_gaf(gaf_lines, evidence_codes, domains):
     headfiltered_gaf = gaf_lines[12:]
     for index, line in enumerate(headfiltered_gaf):
         line_items = line.split("\t")
-        if line_items[3] != "" or line_items[6] not in evidence_codes or line_items[8] not in domains:
+        if line_items[3] != "" or line_items[6] not in evidence_codes or line_items[8] not in domains or line_items[8] == "C":
             del headfiltered_gaf[index]
     return headfiltered_gaf
