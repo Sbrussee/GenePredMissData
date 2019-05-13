d67c7c7adb17380f3c350df0f8bddf655e784658
Jesper Gaming-desk <jjjesper.guijt@gmail.com>
2019-05-13 20:47:13 +0200

    added more compatibility and added documentation

--- GenePredMissData_1apr/classes/filter_gaf.py
+++ GenePredMissData_1apr/classes/filter_gaf.py
@@ -1,5 +1,14 @@
+"""
+Filtergaf removes the header of a gaf file (lines starting with !)
+Also removes lines from the gaf file if it doesnt have an evidence code or domain.
+"""
+
 def filter_gaf(gaf_lines, evidence_codes, domains):
-    headfiltered_gaf = gaf_lines[12:]
+    number=0
+    for determineheader in gaf_lines[0:200]:
+        if determineheader[0] == "!":
+            number += 1
+    headfiltered_gaf = gaf_lines[number:]
     for index, line in enumerate(headfiltered_gaf):
         line_items = line.split("\t")
         if line_items[3] != "" or line_items[6] not in evidence_codes or line_items[8] not in domains or line_items[8] == "C":
